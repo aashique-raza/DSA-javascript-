73cb254b5be30b2dd7fd008bc3ba095ac2aeafe7
Farhan Raza <farhanraza2239@gmail.com>
2025-08-17 14:59:13 +0530

    find pivot

--- array/prefix-suffix-sum.js
+++ array/prefix-suffix-sum.js
@@ -50,7 +50,58 @@ function subarraySum(nums, k) {
     return result;
 }
 
-console.log(subarraySum([1,2,3,4,5], 5)); // Output: 2 (subarrays: [2,3], [5])
-console.log(subarraySum([1,-1,0], 0)); // Output: 3 (subarrays: [1,-1], [-1,0], [1,-1,0])
-console.log(subarraySum([1,-1], 0)); // Output: 1 (subarray: [1,-1])
-console.log(subarraySum([1, 2, 3], 3)); // Output: 2 (subarrays: [1,2], [3])
+// console.log(subarraySum([1,2,3,4,5], 5)); // Output: 2 (subarrays: [2,3], [5])
+// console.log(subarraySum([1,-1,0], 0)); // Output: 3 (subarrays: [1,-1], [-1,0], [1,-1,0])
+// console.log(subarraySum([1,-1], 0)); // Output: 1 (subarray: [1,-1])
+// console.log(subarraySum([1, 2, 3], 3)); // Output: 2 (subarrays: [1,2], [3])
+
+// find pivot index
+
+function pivotIndex(nums) {
+    let i=0
+    let j=0;
+    let p=[]
+    // console.log(p);
+    p[0] = nums[0];
+    for(let i=1; i<nums.length; i++) {
+        p[i] = p[i-1] + nums[i];
+    }
+    while(j<nums.length) {
+        let rightsum=0;
+        let leftsum=0;
+
+        if(j==nums.length-1) {
+            rightsum = 0;
+        }
+        else{
+            let R=nums.length -1
+            let L=j+1
+            rightsum = p[R] - (L>0? p[L-1]:0)
+            
+            
+            ;
+        }
+        if(j==0) {
+            leftsum = 0;
+        }   
+        else {
+            let R= j-1
+            
+            leftsum = p[R]  ;
+        }
+        if(leftsum == rightsum) {
+            console.log("leftsum", leftsum, "rightsum", rightsum);
+            return j;
+        }
+
+
+        j++;
+    }
+
+    return -1; // No pivot index found
+}
+
+console.log(pivotIndex([1,7,3,6,5,6])); // Output: 3 (index of pivot)
+console.log(pivotIndex([1,2,3])); // Output: -1 (no pivot index)
+console.log(pivotIndex([2,1,-1])); // Output: 0 (index of pivot) 
+console.log(pivotIndex([1,2,3,4,5])); // Output: -1 (no pivot index)
