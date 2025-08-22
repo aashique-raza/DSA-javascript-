845980cb9210a54f5abadedae22b1fc1586e2c49
Farhan Raza <farhanraza2239@gmail.com>
2025-08-22 20:03:08 +0530

    optimize approach

--- array/Maximum Sum Circular Subarray.js
+++ array/Maximum Sum Circular Subarray.js
@@ -90,6 +90,78 @@ function circularSubArray(nums){
     return max;
 }
 
-console.log(circularSubArray([5,-3,5]));   // 10
-console.log(circularSubArray([1,-2,3,-2])); // 3
-console.log(circularSubArray([-3,-2,-3]));  // -2
+// console.log(circularSubArray([5,-3,5]));   // 10
+// console.log(circularSubArray([1,-2,3,-2])); // 3
+// console.log(circularSubArray([-3,-2,-3]));  // -2
+
+
+
+//  * approach 2 optimal ---
+
+function maxSubArray(nums) {
+
+    let max= -Infinity; // Initialize max to the smallest possible value
+    let currentSum= 0; // Initialize current sum to the smallest possible value
+
+    for(let i=0; i<nums.length; i++){
+        currentSum += nums[i];
+        max = Math.max(max, currentSum);
+       if(currentSum < 0) {
+            currentSum = 0; // Reset current sum if it goes negative
+        }
+        
+    }
+    ; // If all elements are negative, return 0
+    return max;
+}
+function minSubArray(nums) {
+    let min = Infinity; 
+    let currentSum = 0; 
+    for(let i=0; i<nums.length; i++){
+        currentSum = Math.min(currentSum + nums[i], nums[i]);
+        min = Math.min(min, currentSum);
+    }
+    return min;
+}
+
+
+function  circularSumOptimal(nums){
+
+    let SUM=0
+
+    // find total sum of nums array---
+
+    for(let i=0; i<nums.length; i++){
+        SUM+=nums[i]
+    }
+    console.log('SUM',SUM)
+
+    let minSum;
+
+    // min sum-----
+    minSum=minSubArray(nums)
+
+    console.log('minSum',minSum)
+
+    let maxSum;
+
+    // max sum---
+    maxSum=maxSubArray(nums)
+
+        console.log('maxSum',maxSum)
+
+    let circular_sum=SUM-minSum
+     console.log('circular_sum',circular_sum)
+
+    if(circular_sum>0){
+        return Math.max(maxSum,circular_sum)
+    }
+
+    return maxSum
+
+
+}
+
+
+console.log(circularSumOptimal([5,-3,5]))
+// console.log(circularSumOptimal([-1,-1,-1]))
