acf205414411f56326ea93f96bd703d8a62c224b
Farhan Raza <farhanraza2239@gmail.com>
2025-08-24 18:02:08 +0530

    Subarray Sums Divisible by K (optimized )

--- array/Subarray Sums Divisible by K.js
+++ array/Subarray Sums Divisible by K.js
@@ -28,4 +28,40 @@ function maxSubarray(nums,k){
    return result
 }
 
-console.log(maxSubarray([4,5,0,-2,-3,1],5))
+// console.log(maxSubarray([4,5,0,-2,-3,1],5))
+
+
+//  optimized approach----
+
+function maxSubarray2(nums,k){
+
+    let reminderMap={}
+
+    let sum=0;
+    let result=0;
+    reminderMap[sum]=1
+
+    for(let i=0; i<nums.length; i++){
+
+        sum=sum+nums[i]
+        console.log('sum',sum)
+
+        let r=sum%k
+
+       
+        if(r<0){
+            r=r+k
+        }
+ 
+        if(reminderMap[r]){
+            result=result+reminderMap[r]
+            reminderMap[r]++
+        }else{
+            reminderMap[r]=1
+        }
+    }
+
+    return result
+}
+
+console.log(maxSubarray2([2,-6,3,1,2,8,2,1],7))
