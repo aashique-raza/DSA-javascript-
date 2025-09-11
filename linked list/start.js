7d92655d8b8b0bb625c9f688362009f97998257f
Farhan Raza <farhanraza2239@gmail.com>
2025-09-11 20:42:26 +0530

    making node

--- linked list/start.js
+++ linked list/start.js
@@ -1,28 +1,44 @@
-function SinglyLinkedListNode(data) {
-    this.data = data;
-    this.next = null;
-}
+//  start linked list---
 
-function SinglyLinkedList() {
-    this.head = null;
-    this.size = 0;
-}
+class Node {
 
-SinglyLinkedList.prototype.isEmpty = function() {
-    return this.size == 0;
-}
-
-SinglyLinkedList.prototype.insert = function(value) {
-    if (this.head === null) { //If first node
-        this.head = new SinglyLinkedListNode(value);
-    } else {
-        var temp = this.head;
-        this.head = new SinglyLinkedListNode(value);
-        this.head.next = temp;
+    constructor(data) {
+        this.data = data;
+        this.next = null;
     }
-    this.size++;
 }
-var sll1 = new SinglyLinkedList();
-sll1.insert(1); // linked list is now: 1 -> null
-sll1.insert(12); // linked list is now: 12 -> 1 -> null
-sll1.insert(20); // linked list is now: 20 -> 12 -> 1 -> null
+
+let node1 = new Node(10);
+let node2 = new Node(20);
+let node3 = new Node(30);
+let node4 = new Node(40);
+node1.next = node2;
+console.log('node1',node1);
+console.log('node 1 data',node1.data);
+console.log('node 2 ka address in node 1',node1.next);
+
+console.log('node2', node2);
+console.log('node3', node3);
+console.log('node4', node4);
+
+//  traversing without head
+
+let tem=node1;
+while(tem!=null){
+    console.log(tem.data);
+    tem=tem.next;
+}
+
+// if lost node 1
+let head=node1;
+node1=null;
+console.log('node1',node1);
+console.log('node2',node2);
+console.log('head',head);
+
+//  traverse with head
+let temp=head;  
+while(temp!=null){
+    console.log(temp.data);
+    temp=temp.next;
+}
