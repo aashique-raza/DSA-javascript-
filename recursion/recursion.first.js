4c9605b461a7b4213a33c94ad3b690308e486de0
Farhan Raza <farhanraza2239@gmail.com>
2025-09-18 20:29:15 +0530

    done

--- recursion/recursion.first.js
+++ recursion/recursion.first.js
@@ -1,6 +1,6 @@
 console.log('recursion.first.js');
 
-function sum(n){
+function printN(n){
     if(n==0){
         return 0
     }
@@ -8,4 +8,29 @@ function sum(n){
     return sum(n-1)
 }
 
+printN(5)
+
+
+
+//  ! sum of n natural numbers
+
+
+function sum(n){
+
+    if(n==0) return 0
+    return n + sum(n-1)
+}
+
 console.log('sum',sum(5));
+
+
+
+//  ! print 1 to n
+
+function print1toN(n){
+
+    if(n==10) return
+    console.log(n);
+    print1toN(n+1)
+}
+print1toN(1)
