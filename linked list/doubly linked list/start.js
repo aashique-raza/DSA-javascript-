594c947d05c342e4cea5172e4db2d3ef9fc797d8
Farhan Raza <farhanraza2239@gmail.com>
2025-09-23 11:10:20 +0530

    insertAtStart

--- linked list/doubly linked list/start.js
+++ linked list/doubly linked list/start.js
@@ -1,3 +1,48 @@
 
 
 console.log("Doubly Linked List");
+
+
+// Node class
+
+ class Node{
+    constructor(val){
+        this.val=val;
+        this.prev=null;
+        this.next=null;
+    }
+ }
+
+ class DoublyLinkedList{
+
+     head=null;
+     tail=null;
+     length=0;
+
+    //    insert at start (begining)
+
+    insertAtStart(val){
+
+        if(this.head==null){
+            this.head=new Node(val);
+            this.tail=this.head;
+        }else{
+            let newNode=new Node(val);
+            this.head.prev=newNode;
+            newNode.next=this.head;
+            this.head=newNode;
+        }
+        this.length++;
+    }
+ }
+
+
+//  insert node at start
+    let dll=new DoublyLinkedList();
+
+    dll.insertAtStart(10);
+    dll.insertAtStart(20);
+    dll.insertAtStart(30);
+
+    console.log('head',dll.head);
+    console.log('tail',dll.tail);
