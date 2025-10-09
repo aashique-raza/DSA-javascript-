dd3e36a6ef3cbf87e62b55fa238db90110f1043f
Farhan Raza <farhanraza2239@gmail.com>
2025-10-09 12:57:21 +0530

    hash map with collision

--- hashmap/start.js
+++ hashmap/start.js
@@ -1,3 +1,48 @@
 
 
 console.log("hashmap start");
+
+
+// ! hashmap start here
+
+class HashMap {
+
+    constructor(size){
+
+        this.data = new Array(size);
+    }
+
+    hash(key){
+        let hash=0;
+        for(let i=0; i<key.length; i++){
+            hash = (hash + key.charCodeAt(i) ) % this.data.length;
+        }
+        return hash;
+    }
+
+    set(key , value){
+
+        let add= this.hash(key);
+        this.data[add] = [key, value];
+    }
+
+    get(key){
+        let add = this.hash(key);
+        return this.data[add][1];
+    }
+
+    
+}
+
+let myHashMap = new HashMap(50);
+console.log('myhashmap',myHashMap);
+console.log(417%50);
+console.log('hash for name',myHashMap.hash('name'));
+
+myHashMap.set('name', 'john');
+myHashMap.set('age', 23);
+console.log('myhashmap after set',myHashMap);
+console.log('get name',myHashMap.get('name'));
+ myHashMap.set('mean', 'doe');
+ console.log('get mean',myHashMap.get('mean'));
+
