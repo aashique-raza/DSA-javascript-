eec4f2b15d6f86c04ad956733070307d5fdb1f91
Farhan Raza <farhanraza2239@gmail.com>
2025-10-10 16:01:58 +0530

    approach 2

--- queue/implement queue using stack.js
+++ queue/implement queue using stack.js
@@ -38,3 +38,57 @@ queue.push(2);
 console.log(queue.peek()); // returns 1
 console.log(queue.pop());  // returns 1
 console.log(queue.empty()); // returns false
+
+
+
+var MyQueue = function() {
+    this.mainStack=[]
+    this.helperStack=[]
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+   
+        while(this.mainStack.length!==0){
+            this.helperStack.push(this.mainStack.pop())
+        }
+        this.mainStack.push(x)
+
+        while(this.helperStack.length!=0){
+            this.mainStack.push(this.helperStack.pop())
+        }
+    
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+   return this.mainStack.pop()
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    return this.mainStack[this.mainStack.length-1]
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.mainStack.length===0
+};
+
+/** 
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
