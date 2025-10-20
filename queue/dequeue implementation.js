31110eda159ff0ca7f7eb77ce0ceae7b35dc70ba
Farhan Raza <farhanraza2239@gmail.com>
2025-10-20 11:15:56 +0530

    dequeue implementation using  doubly linked list to avoid traversing the list in removeRear method

--- queue/dequeue implementation.js
+++ queue/dequeue implementation.js
@@ -100,3 +100,88 @@ dq.addRear(10);
 dq.addRear(20);
 dq.addFront(5);
 dq.display();
+
+
+//  ! dequeue implementation using  doubly linked list to avoid traversing the list in removeRear method
+
+class DNode{    
+    constructor(data){
+        this.data=data;
+        this.next=null;
+        this.prev=null;
+    }
+}
+
+class Deque{
+    constructor(){
+        this.front=null;
+        this.rear=null;
+        // use dummy nodes for front and rear to avoid edge cases
+        this.front=new DNode(null);
+        this.rear=new DNode(null);
+        this.front.next=this.rear;
+        this.rear.prev=this.front;
+    }
+    addFront(data){
+        let newNode=new DNode(data);
+        newNode.next=this.front.next;
+        newNode.prev=this.front;
+        this.front.next.prev=newNode;
+        this.front.next=newNode;
+    }
+    addRear(data){
+        let newNode=new DNode(data);
+        newNode.prev=this.rear.prev;    
+        newNode.next=this.rear;
+        this.rear.prev.next=newNode;
+        this.rear.prev=newNode;
+    }
+    removeFront(){
+        if(this.isEmpty()){
+            return null;
+        }
+        let removedData=this.front.next.data;
+        this.front.next=this.front.next.next;
+        this.front.next.prev=this.front;
+        return removedData;
+    }
+    removeRear(){
+        if(this.isEmpty()){
+            return null;
+        }
+        let removedData=this.rear.prev.data;
+        this.rear.prev=this.rear.prev.prev;
+        this.rear.prev.next=this.rear;
+        return removedData;
+    }
+    isEmpty(){
+        return this.front.next===this.rear;
+    }
+    peekFront(){
+        if(this.isEmpty()){
+            return null;
+        }
+        return this.front.next.data;
+    }
+    peekRear(){
+        if(this.isEmpty()){
+            return null;
+        }
+        return this.rear.prev.data;
+    }   
+    display(){
+        let current=this.front.next;
+        let elements=[];
+        while(current!==this.rear){
+            elements.push(current.data);
+            current=current.next;
+        }
+        console.log(elements.join(" <-> "));
+    }
+}
+
+let dq2=new Deque();
+dq2.addRear(10);
+dq2.addRear(20);
+dq2.addFront(5);
+dq2.display();
