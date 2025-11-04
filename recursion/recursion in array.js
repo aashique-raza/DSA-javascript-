0fed4851afcd1579e7b34fac9f1aecdfafb0307c
Farhan Raza <farhanraza2239@gmail.com>
2025-11-04 15:09:01 +0530

    print array reverse using recursion  1

--- recursion/recursion in array.js
+++ recursion/recursion in array.js
@@ -16,3 +16,15 @@ function printArrayElements(arr, index = 0) {
 // Example usage:
 const myArray = [10, 20, 30, 40, 50];
 printArrayElements(myArray);
+
+//  print array reverse using recursion
+function printArrayReverse(arr, index = arr.length - 1) {
+  // base case
+  if (index < 0) {
+    return;
+  }
+    console.log(arr[index]);
+    printArrayReverse(arr, index - 1); 
+}
+// Example usage:
+printArrayReverse(myArray);
