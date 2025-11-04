3acc843fac14a8cc64913e028c3fea0e055254f4
Farhan Raza <farhanraza2239@gmail.com>
2025-11-04 17:29:51 +0530

    reverse string using recursion way 1

--- recursion/recursion in string.js
+++ recursion/recursion in string.js
@@ -38,6 +38,32 @@ function countVowels(str, index = 0,count = 0 ) {
 
 }
 // Example usage:
-const myString = "Hello World";
-const vowelCount = countVowels(myString);
-console.log(`Number of vowels in "${myString}":`, vowelCount);
+// const myString = "Hello World";
+// const vowelCount = countVowels(myString);
+// console.log(`Number of vowels in "${myString}":`, vowelCount);
+
+// ! count consonants in string using recursion way 2
+
+function countConsonants(str, index = 0) {  
+    if(index >= str.length) {
+        return 0;
+    }
+    const vowels = "aeiouAEIOU";
+    let count = 0;
+    if(/[a-zA-Z]/.test(str[index]) && !vowels.includes(str[index])) {
+        count = 1;
+    }
+    return count + countConsonants(str, index + 1);
+}
+
+
+// ! reverse string using recursion way 1
+
+function reverseString(str, index = str.length - 1  ){
+
+    if(index < 0) {
+        return "";
+    }
+    return str[index] + reverseString(str, index - 1);
+} 
+// Example usage:
