e4aefd8370a700d25a381c44660596c731a24dbe
Farhan Raza <farhanraza2239@gmail.com>
2025-11-04 16:50:39 +0530

    count vowels in string using recursion

--- recursion/recursion in string.js
+++ recursion/recursion in string.js
@@ -18,6 +18,26 @@ function isPalindrome(str, left = 0, right = str.length - 1) {
     return true;
 }
 // Example usage:
-const myString = "racecar";
-const result = isPalindrome(myString);
-console.log(`Is "${myString}" a palindrome?`, result);
+// const myString = "racecar";
+// const result = isPalindrome(myString);
+// console.log(`Is "${myString}" a palindrome?`, result);
+
+// ! count vowels in string using recursion
+
+function countVowels(str, index = 0,count = 0 ) {
+
+    if(index >= str.length) {
+        return count;
+    }
+
+    const vowels = "aeiouAEIOU";
+    if(vowels.includes(str[index])) {
+        count++;
+    }
+    return countVowels(str, index + 1, count);
+
+}
+// Example usage:
+const myString = "Hello World";
+const vowelCount = countVowels(myString);
+console.log(`Number of vowels in "${myString}":`, vowelCount);
