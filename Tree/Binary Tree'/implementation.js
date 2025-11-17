c51eb393ee0667e2cb86605c3cc281c9e8795983
farhan ansari <farhanraza2239@gmail.com>
2025-11-17 12:27:40 +0530

    add operation complete

--- Tree/Binary Tree'/implementation.js
+++ Tree/Binary Tree'/implementation.js
@@ -22,9 +22,36 @@ class BinaryTree{
             this.root=newNode
             return this.root
         }
+
+        let queue=[this.root]
+
+        while(queue.length>0){
+            let current=queue.shift()
+
+            if(current.left==null){
+                current.left=newNode
+                return this.root
+            }else{
+                queue.push(current.left)
+            }
+
+            if(current.right==null){
+                current.right=newNode
+                return this.root
+            }else{
+                queue.push(current.right)
+            }
+        }
     }
 }
 
 let binaryTree=new BinaryTree()
+let root=binaryTree.add(10)
+binaryTree.add(20)
+binaryTree.add(30)
+binaryTree.add(40)
+binaryTree.add(50)
+binaryTree.add(60)
+binaryTree.add(70)
 
-console.log("root",binaryTree.add(10))
+console.log("root",root)
