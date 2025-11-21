cb32d32268a1e0c8c2e8465910e4b364e5118420
farhan ansari <farhanraza2239@gmail.com>
2025-11-21 10:38:57 +0530

    count leaves node method 1

---
+++ Tree/Binary Tree'/leaves node.js
@@ -0,0 +1,157 @@
+
+//  ! leaves node ---
+
+class Node{
+    constructor(val){
+        this.val=val
+        this.left=null;
+        this.right=null
+    }
+}
+
+class BinaryTree{
+    constructor(){
+        this.root=null
+    }
+
+    add(val){
+        let newNode=new Node(val)
+        if(this.root==null){
+            this.root=newNode
+            return this.root
+        }
+
+        let queue=[this.root]
+
+        while(queue.length>0){
+            let current=queue.shift()
+
+            if(current.left==null){
+                current.left=newNode
+                return this.root
+            }else{
+                queue.push(current.left)
+            }
+
+            if(current.right==null){
+                current.right=newNode
+                return this.root
+            }else{
+                queue.push(current.right)
+            }
+        }
+    }
+
+    search(searchVal){
+        if(this.root==null){
+            return false
+        }
+
+        let queue=[this.root]
+
+        while(queue.length>0){
+            let curr=queue.shift()
+
+            if(curr.val===searchVal){
+                return true
+            }
+
+            if(curr.left){
+                queue.push(curr.left)
+            }
+            if(curr.right){
+                queue.push(curr.right)
+            }
+        }
+        return false
+    }
+
+     searchVal(search,root=this.root){
+
+        if(root==null ){
+            return false
+        }
+        if(root.val==search){
+            return true
+        }
+
+        return this.searchVal(search, root.left) || this.searchVal(search, root.right);
+    }
+
+    preOrder(root=this.root){
+        if(root==null){
+            return
+        }
+        console.log(root.val)
+        this.preOrder(root.left)
+        this.preOrder(root.right)
+    }
+
+     inOrder(root=this.root){
+        if(root==null) return
+
+        this.inOrder(root.left)
+        console.log(root.val)
+        this.inOrder(root.right)
+    }
+
+    postOrder(root=this.root){
+        if(root==null) return
+
+        this.postOrder(root.left)
+        this.postOrder(root.right)
+        console.log(root.val)
+    }
+
+    // BFS traversal(level order)
+
+    levelOrder(){
+        if(this.root==null) return
+
+        let queue=[this.root]
+
+        while(queue.length>0){
+            let curr=queue.shift()
+            console.log(curr.val)
+
+            if(curr.left!=null) queue.push(curr.left);
+            if(curr.right!=null) queue.push(curr.right)  ;  
+        }
+    }
+   
+
+    
+
+
+   
+}
+
+let binaryTree=new BinaryTree()
+let root=binaryTree.add(10)
+binaryTree.add(20)
+binaryTree.add(30)
+binaryTree.add(40)
+binaryTree.add(50)
+binaryTree.add(60)
+binaryTree.add(70)
+
+
+//  method 1 --
+
+function leavesNode(root){
+    if(root==null) return
+
+    let leaf=0;
+    let queue=[root]
+    while(queue.length>0){
+        let curr=queue.shift()
+
+        if(curr.left) queue.push(curr.left)
+        if(curr.right) queue.push(curr.right)
+         
+            if(curr.left==null && curr.right==null) leaf++
+    }
+    return leaf
+}
+
+console.log ('leaf node',leavesNode(root))
