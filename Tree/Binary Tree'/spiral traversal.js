b806a3779f74f9f988115edbdc28cf02893cbc4b
farhan ansari <farhanraza2239@gmail.com>
2025-11-24 09:24:24 +0530

    modify

--- Tree/Binary Tree'/spiral traversal.js
+++ Tree/Binary Tree'/spiral traversal.js
@@ -1,30 +1,63 @@
 // ! spiral traverse---
 
 
+// function spiralTraverse(root){
+//     let s1=[]
+//     let s2=[]
+//     let result=[]
+
+//     s1.push(root)
+
+//     while(s1.length!==0 || s2.length!==0){
+
+//         if(s1.length!=0){
+//             while(s1.length!==0){
+//                 let temp=s1.pop()
+//                 result.push(temp.val)
+//                 if(temp.right) s2.push(temp.right)
+//                 if(temp.left) s2.push(temp.left)    
+//             }
+//         }else{
+//             while(s2.length!=0){
+//                 let temp=s2.pop()
+//                 result.push(temp.val)
+//                 if(temp.left)s1.push(temp.left)
+//                 if(temp.right)s1.push(temp.right)    
+//             }
+//         }
+//     }
+//     return result
+// }
+
 function spiralTraverse(root){
+    if(root==null)return []
     let s1=[]
     let s2=[]
     let result=[]
+    
 
     s1.push(root)
 
     while(s1.length!==0 || s2.length!==0){
-
+        let temp=[]
         if(s1.length!=0){
             while(s1.length!==0){
                 let temp=s1.pop()
-                result.push(temp.val)
+                temp.push(temp.val)
+                 if(temp.left) s2.push(temp.left) 
                 if(temp.right) s2.push(temp.right)
-                if(temp.left) s2.push(temp.left)    
+                  
             }
         }else{
             while(s2.length!=0){
                 let temp=s2.pop()
-                result.push(temp.val)
+                temp.push(temp.val)
+                if(temp.right)s1.push(temp.right) 
                 if(temp.left)s1.push(temp.left)
-                if(temp.right)s1.push(temp.right)    
+                   
             }
         }
+        result.push(temp)
     }
     return result
 }
