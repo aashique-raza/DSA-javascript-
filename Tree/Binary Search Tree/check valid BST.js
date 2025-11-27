ef9c5d477711de84ebaebfc477d3fa63f76057b0
farhan ansari <farhanraza2239@gmail.com>
2025-11-27 10:36:11 +0530

    check valid BST

---
+++ Tree/Binary Search Tree/check valid BST.js
@@ -0,0 +1,31 @@
+
+
+// ! check valid BSt---
+
+function inOrder(root,result=[]){
+    if(root==null) return
+
+ inOrder(root.left,result)
+ result.push(root.val)
+ inOrder(root.right,result)
+ return result
+}
+
+function checkBST(root){
+    if(root==null)return
+
+    let arr=inOrder(root)
+
+    let i=0;
+
+    while(i<arr.length-1){
+
+        if(arr[i+1]<=arr[i]){
+            return false
+        }
+        i++
+    }
+
+
+    return true
+}
