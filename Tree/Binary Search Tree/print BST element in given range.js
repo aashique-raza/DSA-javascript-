7afed1cde8ca5c71f7fa703d23268a5aee7125b7
farhan ansari <farhanraza2239@gmail.com>
2025-12-04 09:20:04 +0530

    rangeSumBST

--- Tree/Binary Search Tree/print BST element in given range.js
+++ Tree/Binary Search Tree/print BST element in given range.js
@@ -23,3 +23,27 @@ function printBst(root, l, h) {
   storeResult(root, l, h);
   return result;
 }
+
+
+
+var rangeSumBST = function(root, l, h) {
+    if (root == null) return 0;
+
+  let result = 0;
+
+  function storeResult(root, l, h) {
+    if (root == null) return;
+
+    if (root.val > l) {
+      storeResult(root.left, l, h);
+    }
+    if (root.val >= l && root.val <= h) {
+      result+=root.val;
+    }
+    if (root.val < h) {
+      storeResult(root.right, l, h);
+    }
+  }
+  storeResult(root, l, h);
+  return result;
+};
