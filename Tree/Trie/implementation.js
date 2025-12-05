fb06587cf53c0f89ee2c4ff1b58ff4b31f2bd851
farhan ansari <farhanraza2239@gmail.com>
2025-12-05 16:30:53 +0530

    insert in trie tree

--- Tree/Trie/implementation.js
+++ Tree/Trie/implementation.js
@@ -4,8 +4,9 @@
 
 class TrieNode{
     constructor(){
+        
         this.children={}
-        this.isWordComplete=false
+        this.isEndOfWord=false
     }
 }
 
@@ -13,4 +14,23 @@ class Trie{
     constructor(){
         this.root=new TrieNode()
     }
+
+    insert(word){
+        
+        let curr=this.root
+        for(let i =0; i<word.length; i++){
+            if(!curr.children[word[i]]){
+                curr.children[word[i]]=new TrieNode()
+            }
+            curr=curr.children[word[i]]
+        }
+        curr.isEndOfWord=true;
+        return this.root
+    }
+   
 }
+
+
+const myTrie = new Trie();
+console.log('root',myTrie.insert('cat'))
+// console.log("Trie initialized:", myTrie);
