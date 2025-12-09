6f5e3ff2f344d1065bf5ca252ee0d362cbff3384
farhan ansari <farhanraza2239@gmail.com>
2025-12-09 11:11:39 +0530

    modify

--- Tree/Binary Tree'/find height.js
+++ Tree/Binary Tree'/find height.js
@@ -14,3 +14,26 @@ function height(root){
 
     return Math.max(leftHeight,rightHeight)+1
 }
+
+
+//  approach 2 : BFS
+
+function height1(root){
+    if(!root) return 0
+
+    let queue=[root]
+    let height=0
+
+    while(queue.length>0){
+        let size=queue.length
+
+        for(let i=0; i<size; i++){
+            let curr=queue.shift()
+
+            if(curr.left) queue.push(curr.left)
+            if(curr.right) queue.push(curr.right)    
+        }
+    height++
+    }
+    return height
+}
