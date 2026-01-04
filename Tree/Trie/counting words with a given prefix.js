b2f03e7fb6cd8dc1b3cf2d47d911de6bbc72770d
farhan ansari <farhanraza2239@gmail.com>
2026-01-04 11:40:54 +0530

    approach 2 using trie

--- Tree/Trie/counting words with a given prefix.js
+++ Tree/Trie/counting words with a given prefix.js
@@ -9,3 +9,62 @@ var prefixCount = function(words, pref) {
     }
     return count
 };
+
+
+// approach 2 using trie
+
+class TrieNode {
+  constructor() {
+    this.children = {};
+    this.isEndOfWord = false;
+    this.count = 0; // To keep track of the number of words passing through this node
+  }
+}
+
+class Trie {
+  constructor() {
+    this.root = new TrieNode();
+  }
+
+  insert(word) {
+    let curr = this.root;
+    for (let i = 0; i < word.length; i++) {
+      if (!curr.children[word[i]]) {
+        curr.children[word[i]] = new TrieNode();
+        curr.children[word[i]].count = 1; // First time this character is added
+      }
+    else{
+        curr.children[word[i]].count += 1; // Increment count for existing character
+    }
+      curr = curr.children[word[i]];
+    }
+    curr.isEndOfWord = true;
+    return this.root;
+  }
+
+  search(word) {
+    let curr = this.root;
+    let prefixCount = 0;
+    for (let i = 0; i < word.length; i++) {
+      let char = word[i];
+
+      if (!curr.children[char]) {
+        return 0;
+      }
+
+      curr = curr.children[char];
+        prefixCount = curr.count;
+    }
+    return prefixCount;
+  }
+}
+
+var prefixCount = function(words, pref) {
+   let trie=new Trie()
+
+//    ds build--
+for(let i=0; i<words.length; i++){
+    trie.insert(words[i])
+}
+return trie.search(pref)
+};
