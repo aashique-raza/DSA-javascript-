2500ee8380290bea96954bf392b1e40b17faa46d
farhan ansari <farhanraza2239@gmail.com>
2026-01-07 11:30:06 +0530

    koko eating bnana approach 2 optimal approach using binary search

--- Binary Search/koko eating bnana.js
+++ Binary Search/koko eating bnana.js
@@ -18,3 +18,25 @@ var minEatingSpeed = function(piles, h) {
     }
     return minSpeed;
 }
+
+//  approach 2 optimal approach using binary search
+var minEatingSpeed = function(piles, h) {
+    let left=1;
+    let right=Math.max(...piles);
+    let minSpeed=right;
+    while(left<=right){
+        let mid=Math.floor((left+right)/2);
+        let totalHours=0;
+        for(let i=0;i<piles.length;i++){
+            totalHours+=Math.ceil(piles[i]/mid)
+        }
+        if(totalHours<=h){
+            minSpeed=mid
+            right=mid-1
+        }
+        else{
+            left=mid+1
+        }
+    }
+    return minSpeed;
+}
