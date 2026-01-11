6317d4656a18b480f35f80eb29e201d5244f1b67
farhan ansari <farhanraza2239@gmail.com>
2026-01-11 09:38:41 +0530

    climb stiars approach 2 using array

--- DP/Climbing Stairs.js
+++ DP/Climbing Stairs.js
@@ -22,4 +22,20 @@ var climbStairs = function(n) {
 
 };
 
+//  using array in dp
+var climbStairs = function(n) {
+    
+    let dp=new Array(n+1).fill(0)
+
+    dp[0]=0
+    dp[1]=1
+    dp[2]=2
+
+    for(let i=3;i<=n;i++){
+        dp[i]=dp[i-1]+dp[i-2]
+    }
+
+    return dp[n]
+};
+
 
