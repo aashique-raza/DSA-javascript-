6e883ef8b34b577a0082392a82809979056d87e8
farhan ansari <farhanraza2239@gmail.com>
2026-01-12 10:55:06 +0530

    frog jump dp optimization

--- DP/Frog Jump.js
+++ DP/Frog Jump.js
@@ -26,3 +26,33 @@ function canCross(stones){
 
    return dfs(0, 0);
 }
+
+
+//  using dp and recursion --
+function canCross(stones) {
+
+    let map = new Map();
+    for (let i = 0; i < stones.length; i++) {
+        map.set(stones[i], i);
+    }
+    let dp = new Array(stones.length).fill(0).map(() => new Array(stones.length).fill(-1));
+
+    function dfs(idx, jump) {
+        if (idx === stones.length - 1) return true;
+        if (dp[idx][jump] !== -1) return dp[idx][jump] === 1 ? true : false;
+        let result = false;
+        for (let step = jump - 1; step <= jump + 1; step++) {
+            if (step > 0) {
+                let nextPos = stones[idx] + step;
+                if (map.has(nextPos)) {
+                    let nextIdx = map.get(nextPos);
+                    result = result || dfs(nextIdx, step);
+                }
+            }
+        }
+        dp[idx][jump] = result ? 1 : 0;
+        return result;
+    }
+
+    return dfs(0, 0);
+}
