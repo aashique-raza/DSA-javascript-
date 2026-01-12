958aef2d8bbf6f717dd96633719e30a3c7c8d221
farhan ansari <farhanraza2239@gmail.com>
2026-01-12 15:47:49 +0530

    approach 2 using recursion

--- DP/minimum cost climbing stairs.js
+++ DP/minimum cost climbing stairs.js
@@ -12,3 +12,19 @@ function minCostClimbingStairs(cost) {
     return dp[n];
 
 }
+
+
+//  approach 2 using recursion and memoization
+function minCostClimbingStairs(cost) {
+    const n = cost.length;
+    const dp = new Array(n + 1).fill(-1);
+    function helper(i) {
+        if (i <= 1) return 0;
+        if (dp[i] !== -1) return dp[i];
+        const takeOneStep = helper(i - 1) + cost[i - 1];
+        const takeTwoSteps = helper(i - 2) + cost[i - 2];
+        dp[i] = Math.min(takeOneStep, takeTwoSteps);
+        return dp[i];
+    }
+    return helper(n);
+}
