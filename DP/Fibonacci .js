b1a1cda209921719d7a03d635c65f98f6458d085
farhan ansari <farhanraza2239@gmail.com>
2026-01-13 09:45:18 +0530

    using tabulation

--- DP/Fibonacci .js
+++ DP/Fibonacci .js
@@ -23,3 +23,15 @@ function fib(n, dp = []) {
 
   return dp[n];
 }
+
+
+// using tabulation
+function fibTab(n) {
+  const dp = new Array(n + 1).fill(0);
+  dp[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+}
+console.log(fib(6)); // Output: 8
