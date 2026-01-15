ea90ccc97bf0ec6918555252572ae88f213746d8
farhan ansari <farhanraza2239@gmail.com>
2026-01-15 10:15:56 +0530

    house robber 2 with tabulation

--- DP/House robber2.js
+++ DP/House robber2.js
@@ -46,3 +46,45 @@ function houseRobber(nums){
     let skipFirst=solve(1,n-1); // excluding first element
     return Math.max(takeFirst,skipFirst);
 }
+
+
+//  house robber 2 with tabulation--
+
+function houseRobber(nums){
+
+    if(nums.length===1){
+        return nums[0];
+    }
+    if(nums.length===0){
+        return 0;
+    }
+    if(nums.length===2){
+        return Math.max(nums[0],nums[1]);
+    }
+    let n=nums.length;
+    let dp1=new Array(n-1).fill(0); // for excluding last element
+
+    let max=0;
+    //  first pick index 0 to n-2
+
+    for(let i=0;i<n-1;i++){
+        // handle index out bound with 0
+        let pick=nums[i]+(i-2>=0 ? dp1[i-2]:0);
+        let notPick= (i-1>=0 ? dp1[i-1]:0);
+        dp1[i]=Math.max(pick,notPick);
+    }
+    max= Math.max(...dp1);;
+    dp1.fill(0);
+
+    //  now pick index 1 to n-1
+    for(let i=1;i<n;i++){
+        // handle index out bound with 0
+        let pick=nums[i]+(i-2>=1 ? dp1[i-2]:0);
+        let notPick= (i-1>=1 ? dp1[i-1]:0);
+        dp1[i]=Math.max(pick,notPick);
+    }
+    max= Math.max(max, ...dp1);
+    return max;
+
+}
+
