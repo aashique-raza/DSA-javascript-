f0a65a88599122aade33338b322769d1b0d94b55
farhan ansari <farhanraza2239@gmail.com>
2026-01-15 09:28:41 +0530

    house robber 2 recursion with dp

--- DP/House robber2.js
+++ DP/House robber2.js
@@ -23,3 +23,26 @@ function houseRobber(nums){
     return Math.max(takeFirst,skipFirst);
 }
 
+//  recursion with dp-- memoization--
+function houseRobber(nums){
+    let n=nums.length;
+    let dp=new Array(n).fill(-1);
+    function solve(i,n){
+        if(i>n){
+            return 0;
+        }
+        if(dp[i]!==-1){
+            return dp[i];
+        }
+        // include
+        let include=nums[i]+solve(i+2,n);   
+        // exclude
+        let exclude=solve(i+1,n);
+        dp[i]=Math.max(include,exclude);
+        return dp[i];
+    }
+    let takeFirst=solve(0,n-2); // excluding last element
+    dp.fill(-1);
+    let skipFirst=solve(1,n-1); // excluding first element
+    return Math.max(takeFirst,skipFirst);
+}
